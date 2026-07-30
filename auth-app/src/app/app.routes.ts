17036ec82f6c0c4c85cca29231bff3147219e45e
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2026-07-30 14:07:58 -0300

    feat: define rotas para login, registro e dashboard com proteção de acesso

--- auth-app/src/app/app.routes.ts
+++ auth-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { guestOnlyGuard, authGuard } from './core/guards/auth.guard';
+import { Dashboard } from './dashboard/dashboard';
+
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+	{ path: 'login', component: Login, canActivate: [guestOnlyGuard] },
+	{ path: 'register', component: Register, canActivate: [guestOnlyGuard] },
+	{ path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+	{ path: '**', redirectTo: 'login' },
+];
