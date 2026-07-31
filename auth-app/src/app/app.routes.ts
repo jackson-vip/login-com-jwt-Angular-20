2f0146f0f4f5c69f3a62d4e3635fea53fa45cba0
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2026-07-31 16:22:29 -0300

    feat: reorganiza rotas para incluir layouts privados e públicos com proteção de acesso

--- auth-app/src/app/app.routes.ts
+++ auth-app/src/app/app.routes.ts
@@ -3,12 +3,32 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
 import { guestOnlyGuard, authGuard } from './core/guards/auth.guard';
-import { Dashboard } from './dashboard/dashboard';
+import { PrivateLayout } from './layouts/private-layout/private-layout';
+import { PublicLayout } from './layouts/public-layout/public-layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Clients } from './pages/clients/clients';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'login' },
-	{ path: 'login', component: Login, canActivate: [guestOnlyGuard] },
-	{ path: 'register', component: Register, canActivate: [guestOnlyGuard] },
-	{ path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
-	{ path: '**', redirectTo: 'login' },
+	{
+		path: '',
+		component: PrivateLayout,
+		canActivate: [authGuard],
+		children: [
+			{ path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+			{ path: 'dashboard', component: Dashboard },
+			{ path: 'clientes', component: Clients },
+		],
+	},
+	{
+		path: '',
+		component: PublicLayout,
+		canActivate: [guestOnlyGuard],
+		children: [
+			{ path: '', pathMatch: 'full', redirectTo: 'login' },
+			{ path: 'login', component: Login },
+			{ path: 'register', component: Register },
+		],
+	},
+	{ path: '**', redirectTo: 'dashboard' },
 ];
